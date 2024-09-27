e86f0c81c367577ead9c954fc24d4008109dec47
Robin Hayman <r.hayman@ucl.ac.uk>
2024-09-27 16:25:07 +0100

    Added minimum TTL duration

--- RippleDetector.h
+++ RippleDetector.h
@@ -3,132 +3,153 @@
 #define __RIPPLE_DETECTOR_H
 
 #include <ProcessorHeaders.h>
-#include <stdio.h>			 
-#include <iostream>
-#include <string>			  
-#include <vector>
 #include <chrono>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
 
 class RippleDetectorEditor;
 
-class RippleDetectorSettings
-{
+class RippleDetectorSettings {
 public:
+  /** Constructor -- sets default values **/
+  RippleDetectorSettings();
 
-    /** Constructor -- sets default values **/
-    RippleDetectorSettings();
-    
-	/** Destructor **/
-    ~RippleDetectorSettings() {}
-    
-	/** Creates an event associated with ripple detection */
-    TTLEventPtr createEvent(int64 outputLine, int64 sample_number, bool state);
-    
-    // Time-related variables
-    std::chrono::milliseconds refractoryTimeStart;
-    std::chrono::milliseconds timeNow;
+  /** Destructor **/
+  ~RippleDetectorSettings() {}
 
-    // Interface corresponding parameters
-    int rippleInputChannel;                			    //Input channel
-    int rippleOutputChannel;            			    //Output TTL channel
-    int movementInputChannel;                			//Movement detection channel
-    int movementOutputChannel;            				//Movement detection output TTL channel
-    double rippleSds;                					//Number of standard deviations above average to compose the amplitude threshold
-    int timeThreshold;            					    //Time threshold (milliseconds)
-    unsigned int refractoryTime;    					//Refractory time in milliseconds
-    int rmsSamples;                					    //Number of samples in each buffer subdivision for calculating the RMS
-    String movSwitch;            					    //Movement detection switch (on/off)
-    double movSds;        							    //Number of standard deviations above average for EMG/ACC
-    int minTimeWoMov;        							//Minimum time that EMG RMS must be below threshold to enable detection
-    int minTimeWMov;        							//Minimum time that EMG RMS must be above threshold to disable detection
+  /** Creates an event associated with ripple detection */
+  TTLEventPtr createEvent(int64 outputLine, int64 sample_number, bool state);
 
-    // Internal auxiliary variables
-    int numSamplesTimeThreshold;    					//Number of samples corresponding to time threshold according to the sample rate
-    int minMovSamplesBelowThresh;    					//Number of samples corresponding to EMG/ACC minimum time below threshold to enable detection
-    int minMovSamplesAboveThresh;        				//Number of samples corresponding to EMG minimum time above threshold to disable detection
-    unsigned int counterAboveThresh;    				//Accumulate the number of samples when RMS values are above threshold
-    int bufferSize;                        				//Number of pre-allocated samples in each buffer
-    int realNumberOfSamples;        					//Real number of samples in the buffer (usually lesser than buffer size, at least for File Reader tests)
-    int pointsProcessed;            					//Total number of points processed during the calibration step
-    int calibrationPoints;                    			//Total number of points that defines the calibration duration
-    unsigned int counterMovUpThresh;        			//Accumulate the number of samples when RMS values for EMG/ACC are above threshold
-    unsigned int counterMovDownThresh;        			//Accumulate the number of samples when RMS values for EMG/ACC are below threshold
-    std::vector<int> auxChannelIndices;        			//Contains the indices of aux channels. Useful for movement detector when "ACCEL" is selected
+  // Time-related variables
+  std::chrono::milliseconds refractoryTimeStart;
+  std::chrono::milliseconds timeNow;
+  std::chrono::milliseconds rippleStartTime;
 
-    // RMS variables
-    int rmsEndIdx;            							//The end index for RMS calculation windows
-    double rmsMean;            							//Baseline RMS mean for ripple detection
-    double rmsStdDev;            						//Baseline RMS standard deviation for ripple detection
-    double movRmsMean;        							//Baseline RMS mean for EMG/ACC signal
-    double movRmsStdDev;        						//Baseline RMS standard deviation for EMG signal
-    double threshold;        							//Final threshold for ripple detection
-    double movThreshold;    							//Final EMG/ACC threshold for movement detector
+  // Interface corresponding parameters
+  int rippleInputChannel;    // Input channel
+  int rippleOutputChannel;   // Output TTL channel
+  int movementInputChannel;  // Movement detection channel
+  int movementOutputChannel; // Movement detection output TTL channel
+  double rippleSds;  // Number of standard deviations above average to compose
+                     // the amplitude threshold
+  int timeThreshold; // Time threshold (milliseconds)
+  unsigned int refractoryTime; // Refractory time in milliseconds
+  int rmsSamples;   // Number of samples in each buffer subdivision for
+                    // calculating the RMS
+  String movSwitch; // Movement detection switch (on/off)
+  double movSds;    // Number of standard deviations above average for EMG/ACC
+  int minTimeWoMov; // Minimum time that EMG RMS must be below threshold to
+                    // enable detection
+  int minTimeWMov;  // Minimum time that EMG RMS must be above threshold to
+                    // disable detection
 
-    // Event flags
-    bool isCalibrating { true };        				//Is in the calibration step
-    bool pluginEnabled{ true };            				//Plugin is enabled to send TTL events
-    bool onRefractoryTime { false };    				//On refractory time (ripples cannot be detected during the refractory time)
-    bool rippleDetected{ false };        				//Indicates that a ripple was detected
-    bool flagTimeThreshold{ false };    				//Indicates that the time threshold was achieved
-    bool movSwitchEnabled{ false };        				//EMG switching mechanism is enabled
-    bool movChannChanged{ false };        				//User selected new EMG/ACC channel
-    bool calibrate{ false };            				//Indicates the need to recalibrate
-    bool flagMovMinTimeUp{ false };        				//Indicates that the minimum time of EMG/ACC above threshold was achieved
-    bool flagMovMinTimeDown{ false };        			//Indicates that the minimum time of EMG/ACC below threshold was achieved
-    bool accelerometerSelected{ false };    			//True when accelerometer is selected to movement detector
+  // Internal auxiliary variables
+  int numSamplesTimeThreshold;     // Number of samples corresponding to time
+                                   // threshold according to the sample rate
+  int minMovSamplesBelowThresh;    // Number of samples corresponding to EMG/ACC
+                                   // minimum time below threshold to enable
+                                   // detection
+  int minMovSamplesAboveThresh;    // Number of samples corresponding to EMG
+                                   // minimum time above threshold to disable
+                                   // detection
+  unsigned int counterAboveThresh; // Accumulate the number of samples when RMS
+                                   // values are above threshold
+  int bufferSize;          // Number of pre-allocated samples in each buffer
+  int realNumberOfSamples; // Real number of samples in the buffer (usually
+                           // lesser than buffer size, at least for File Reader
+                           // tests)
+  int pointsProcessed;     // Total number of points processed during the
+                           // calibration step
+  int calibrationPoints; // Total number of points that defines the calibration
+                         // duration
+  unsigned int counterMovUpThresh; // Accumulate the number of samples when RMS
+                                   // values for EMG/ACC are above threshold
+  unsigned int
+      counterMovDownThresh; // Accumulate the number of samples when RMS values
+                            // for EMG/ACC are below threshold
+  std::vector<int>
+      auxChannelIndices; // Contains the indices of aux channels. Useful for
+                         // movement detector when "ACCEL" is selected
 
-    // TTL event channel
-    EventChannel* eventChannel;
-    
+  // RMS variables
+  int rmsEndIdx;       // The end index for RMS calculation windows
+  double rmsMean;      // Baseline RMS mean for ripple detection
+  double rmsStdDev;    // Baseline RMS standard deviation for ripple detection
+  double movRmsMean;   // Baseline RMS mean for EMG/ACC signal
+  double movRmsStdDev; // Baseline RMS standard deviation for EMG signal
+  double threshold;    // Final threshold for ripple detection
+  double movThreshold; // Final EMG/ACC threshold for movement detector
+
+  // Event flags
+  bool isCalibrating{true};     // Is in the calibration step
+  bool pluginEnabled{true};     // Plugin is enabled to send TTL events
+  bool onRefractoryTime{false}; // On refractory time (ripples cannot be
+                                // detected during the refractory time)
+  bool rippleDetected{false};   // Indicates that a ripple was detected
+  bool flagTimeThreshold{
+      false}; // Indicates that the time threshold was achieved
+  bool movSwitchEnabled{false};   // EMG switching mechanism is enabled
+  bool movChannChanged{false};    // User selected new EMG/ACC channel
+  bool calibrate{false};          // Indicates the need to recalibrate
+  bool flagMovMinTimeUp{false};   // Indicates that the minimum time of EMG/ACC
+                                  // above threshold was achieved
+  bool flagMovMinTimeDown{false}; // Indicates that the minimum time of EMG/ACC
+                                  // below threshold was achieved
+  bool accelerometerSelected{
+      false}; // True when accelerometer is selected to movement detector
+  double ttl_duration;
+
+  // TTL event channel
+  EventChannel *eventChannel;
 };
 
-class RippleDetector : public GenericProcessor
-{
+class RippleDetector : public GenericProcessor {
 public:
-    
-    /** Constructor */
-    RippleDetector();
-    
-    /** Destructor */
-    ~RippleDetector() {}
-    
-    /** Creates the custom editor for this plugin */
-    AudioProcessorEditor* createEditor() override;
-    
-    /** Emits an event whenever a ripple is detected */
-    void process (AudioBuffer<float>& buffer) override;
-    
-    /** Called when a processor needs to update its settings */
-    void updateSettings() override;
+  /** Constructor */
+  RippleDetector();
 
-    /** Called when a parameter is updated */
-    void parameterValueChanged(Parameter* param) override;
+  /** Destructor */
+  ~RippleDetector() {}
 
-    std::atomic<bool> shouldCalibrate { true };
+  /** Creates the custom editor for this plugin */
+  AudioProcessorEditor *createEditor() override;
+
+  /** Emits an event whenever a ripple is detected */
+  void process(AudioBuffer<float> &buffer) override;
+
+  /** Called when a processor needs to update its settings */
+  void updateSettings() override;
+
+  /** Called when a parameter is updated */
+  void parameterValueChanged(Parameter *param) override;
+
+  std::atomic<bool> shouldCalibrate{true};
 
 private:
+  RippleDetectorEditor *ed;
 
-    RippleDetectorEditor* ed;
-    
-    StreamSettings<RippleDetectorSettings> settings;
+  StreamSettings<RippleDetectorSettings> settings;
 
-    std::map<uint64, std::vector<double>> rmsValuesArray;
-    std::map<uint64, std::vector<int>> rmsNumSamplesArray;
-    std::map<uint64, std::vector<double>> movRmsValuesArray;
-    std::map<uint64, std::vector<int>> movRmsNumSamplesArray;
-    std::map<uint64, std::vector<double>> calibrationRmsValues;
-    std::map<uint64, std::vector<double>> calibrationMovRmsValues;
-    
-    // Ripple-specific functions
-    void finishCalibration(uint64 streamId);
-    void detectRipples(uint64 streamId);
-    void evalMovement(uint64 streamId);
-    
-    double calculateRms(const float* data, int initIndex, int endIndexOpen);
-    double calculateRms(std::vector<float> data, int initIndex, int endIndex);
-    std::vector<float> calculateAccelMod(const float* axis[3], int numberOfSamples);
+  std::map<uint64, std::vector<double>> rmsValuesArray;
+  std::map<uint64, std::vector<int>> rmsNumSamplesArray;
+  std::map<uint64, std::vector<double>> movRmsValuesArray;
+  std::map<uint64, std::vector<int>> movRmsNumSamplesArray;
+  std::map<uint64, std::vector<double>> calibrationRmsValues;
+  std::map<uint64, std::vector<double>> calibrationMovRmsValues;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(RippleDetector);
+  // Ripple-specific functions
+  void finishCalibration(uint64 streamId);
+  void detectRipples(uint64 streamId);
+  void evalMovement(uint64 streamId);
+
+  double calculateRms(const float *data, int initIndex, int endIndexOpen);
+  double calculateRms(std::vector<float> data, int initIndex, int endIndex);
+  std::vector<float> calculateAccelMod(const float *axis[3],
+                                       int numberOfSamples);
+
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(RippleDetector);
 };
 
 #endif
--- RippleDetectorEditor.cpp
+++ RippleDetectorEditor.cpp
@@ -1,125 +1,124 @@
-// v060422
 #include "RippleDetectorEditor.h"
 
-CustomTextBoxParameterEditor::CustomTextBoxParameterEditor(Parameter* param) : ParameterEditor(param)
-{
+CustomTextBoxParameterEditor::CustomTextBoxParameterEditor(Parameter *param)
+    : ParameterEditor(param) {
 
-    jassert(param->getType() == Parameter::FLOAT_PARAM
-        || param->getType() == Parameter::INT_PARAM
-        || param->getType() == Parameter::STRING_PARAM);
+  jassert(param->getType() == Parameter::FLOAT_PARAM ||
+          param->getType() == Parameter::INT_PARAM ||
+          param->getType() == Parameter::STRING_PARAM);
 
-    parameterNameLabel = std::make_unique<Label>("Parameter name", param->getName());
-    Font labelFont = Font("Silkscreen", "Regular", 10);
-    int labelWidth = 120; //labelFont.getStringWidth(param->getName());
-    parameterNameLabel->setFont(labelFont);
-    parameterNameLabel->setColour(Label::textColourId, Colours::darkgrey);
-    addAndMakeVisible(parameterNameLabel.get());
+  parameterNameLabel =
+      std::make_unique<Label>("Parameter name", param->getName());
+  Font labelFont = Font("Silkscreen", "Regular", 10);
+  int labelWidth = 120; // labelFont.getStringWidth(param->getName());
+  parameterNameLabel->setFont(labelFont);
+  parameterNameLabel->setColour(Label::textColourId, Colours::darkgrey);
+  addAndMakeVisible(parameterNameLabel.get());
 
-    if(param->getType() == Parameter::FLOAT_PARAM)
-        valueTextBox = std::make_unique<Label>("Parameter value", String(float(param->getValue())));
-    else
-        valueTextBox = std::make_unique<Label>("Parameter value", param->getValue().toString());
+  if (param->getType() == Parameter::FLOAT_PARAM)
+    valueTextBox = std::make_unique<Label>("Parameter value",
+                                           String(float(param->getValue())));
+  else
+    valueTextBox = std::make_unique<Label>("Parameter value",
+                                           param->getValue().toString());
 
-    valueTextBox->setFont(Font("CP Mono", "Plain", 15));
-    valueTextBox->setName(param->getProcessor()->getName() + " (" + String(param->getProcessor()->getNodeId()) + ") - " + param->getName());
-    valueTextBox->setColour(Label::textColourId, Colours::white);
-    valueTextBox->setColour(Label::backgroundColourId, Colours::grey);
+  valueTextBox->setFont(Font("CP Mono", "Plain", 15));
+  valueTextBox->setName(param->getProcessor()->getName() + " (" +
+                        String(param->getProcessor()->getNodeId()) + ") - " +
+                        param->getName());
+  valueTextBox->setColour(Label::textColourId, Colours::white);
+  valueTextBox->setColour(Label::backgroundColourId, Colours::grey);
+  valueTextBox->setEditable(true);
+  valueTextBox->addListener(this);
+  valueTextBox->setTooltip(param->getDescription());
+  addAndMakeVisible(valueTextBox.get());
+
+  finalWidth = std::max(labelWidth, 80);
+
+  setBounds(0, 0, labelWidth, 18);
+}
+
+void CustomTextBoxParameterEditor::labelTextChanged(Label *label) {
+  if (param->getType() == Parameter::FLOAT_PARAM)
+    param->setNextValue(label->getText().getFloatValue());
+  else
+    param->setNextValue(label->getText());
+}
+
+void CustomTextBoxParameterEditor::updateView() {
+
+  if (param != nullptr) {
     valueTextBox->setEditable(true);
-    valueTextBox->addListener(this);
-    valueTextBox->setTooltip(param->getDescription());
-    addAndMakeVisible(valueTextBox.get());
-    
-    finalWidth = std::max(labelWidth, 80);
 
-    setBounds(0, 0, labelWidth, 18);
-}
-
-void CustomTextBoxParameterEditor::labelTextChanged(Label* label)
-{
-    if(param->getType() == Parameter::FLOAT_PARAM)
-        param->setNextValue(label->getText().getFloatValue());
+    if (param->getType() == Parameter::FLOAT_PARAM)
+      valueTextBox->setText(String(float(param->getValue())),
+                            dontSendNotification);
     else
-        param->setNextValue(label->getText());
+      valueTextBox->setText(param->getValue().toString(), dontSendNotification);
+  } else {
+    valueTextBox->setEditable(false);
+  }
 }
 
-void CustomTextBoxParameterEditor::updateView()
-{
-    
-    if (param != nullptr)
-    {
-        valueTextBox->setEditable(true);
-
-        if(param->getType() == Parameter::FLOAT_PARAM)
-            valueTextBox->setText(String(float(param->getValue())), dontSendNotification);
-        else
-            valueTextBox->setText(param->getValue().toString(), dontSendNotification);
-    }
-    else 
-	{
-        valueTextBox->setEditable(false);
-    }
-
-}
-
-void CustomTextBoxParameterEditor::resized()
-{
-    parameterNameLabel->setBounds(0, 0, int(3 * finalWidth / 5), 18);
-    valueTextBox->setBounds(int(3 * finalWidth / 5), 0, int(2 * finalWidth / 5), 18);
+void CustomTextBoxParameterEditor::resized() {
+  parameterNameLabel->setBounds(0, 0, int(3 * finalWidth / 5), 18);
+  valueTextBox->setBounds(int(3 * finalWidth / 5), 0, int(2 * finalWidth / 5),
+                          18);
 }
 
 // Class constructor
-RippleDetectorEditor::RippleDetectorEditor(GenericProcessor* parentNode)
-	: GenericEditor(parentNode)
-{
-    
-    rippleDetector = (RippleDetector*)parentNode;
-    
-	desiredWidth = 460; //Plugin's desired width`
+RippleDetectorEditor::RippleDetectorEditor(GenericProcessor *parentNode)
+    : GenericEditor(parentNode) {
 
-	/* Ripple Detection Settings */
-	addSelectedChannelsParameterEditor("Ripple_Input", 10, 40);
+  rippleDetector = (RippleDetector *)parentNode;
 
-	addComboBoxParameterEditor("Ripple_Out", 10, 70);
+  desiredWidth = 460; // Plugin's desired width`
 
-	Parameter* param = getProcessor()->getParameter("ripple_std");
-    addCustomParameterEditor(new CustomTextBoxParameterEditor(param), 95, 25);
+  /* Ripple Detection Settings */
+  addSelectedChannelsParameterEditor("Ripple_Input", 10, 25);
 
-	param = getProcessor()->getParameter("time_thresh");
-    addCustomParameterEditor(new CustomTextBoxParameterEditor(param), 95, 50);
+  addComboBoxParameterEditor("Ripple_Out", 10, 50);
 
-	param = getProcessor()->getParameter("refr_time");
-    addCustomParameterEditor(new CustomTextBoxParameterEditor(param), 95, 75);
+  Parameter *param = getProcessor()->getParameter("ripple_std");
+  addCustomParameterEditor(new CustomTextBoxParameterEditor(param), 95, 25);
 
-	param = getProcessor()->getParameter("rms_samples");
-    addCustomParameterEditor(new CustomTextBoxParameterEditor(param), 95, 100);
+  param = getProcessor()->getParameter("time_thresh");
+  addCustomParameterEditor(new CustomTextBoxParameterEditor(param), 95, 50);
 
-	/* EMG / ACC Movement Detection Settings */
-	addComboBoxParameterEditor("mov_detect", 230, 20);
-	addSelectedChannelsParameterEditor("Mov_Input", 230, 65);
-	addComboBoxParameterEditor("Mov_Out", 230, 85);
+  param = getProcessor()->getParameter("ttl_duration");
+  addCustomParameterEditor(new CustomTextBoxParameterEditor(param), 325, 100);
 
-	param = getProcessor()->getParameter("mov_std");
-    addCustomParameterEditor(new CustomTextBoxParameterEditor(param), 325, 25);
+  param = getProcessor()->getParameter("refr_time");
+  addCustomParameterEditor(new CustomTextBoxParameterEditor(param), 95, 75);
 
-	param = getProcessor()->getParameter("min_time_st");
-    addCustomParameterEditor(new CustomTextBoxParameterEditor(param), 325, 50);
+  param = getProcessor()->getParameter("rms_samples");
+  addCustomParameterEditor(new CustomTextBoxParameterEditor(param), 95, 100);
 
-	param = getProcessor()->getParameter("min_time_mov");
-    addCustomParameterEditor(new CustomTextBoxParameterEditor(param), 325, 75);
+  /* EMG / ACC Movement Detection Settings */
+  addComboBoxParameterEditor("mov_detect", 230, 20);
+  addSelectedChannelsParameterEditor("Mov_Input", 230, 65);
+  addComboBoxParameterEditor("Mov_Out", 230, 85);
 
-	/* Calibration Button */
-	calibrateButton = std::make_unique<UtilityButton>("Calibrate", titleFont);
-	calibrateButton->addListener(this);
-    calibrateButton->setRadius(3.0f);
-    calibrateButton->setBounds(335, 100, 100, 25);
-    addAndMakeVisible(calibrateButton.get());
+  param = getProcessor()->getParameter("mov_std");
+  addCustomParameterEditor(new CustomTextBoxParameterEditor(param), 325, 25);
 
+  param = getProcessor()->getParameter("min_time_st");
+  addCustomParameterEditor(new CustomTextBoxParameterEditor(param), 325, 50);
+
+  param = getProcessor()->getParameter("min_time_mov");
+  addCustomParameterEditor(new CustomTextBoxParameterEditor(param), 325, 75);
+
+  /* Calibration Button */
+  calibrateButton = std::make_unique<UtilityButton>("Calibrate", titleFont);
+  calibrateButton->addListener(this);
+  calibrateButton->setRadius(3.0f);
+  calibrateButton->setBounds(10, 100, 80, 25);
+  addAndMakeVisible(calibrateButton.get());
 }
 
-void RippleDetectorEditor::buttonClicked(Button*)
-{
-	/* Calibration button was clicked */
-	rippleDetector->shouldCalibrate = true;
+void RippleDetectorEditor::buttonClicked(Button *) {
+  /* Calibration button was clicked */
+  rippleDetector->shouldCalibrate = true;
 }
 
 // Called when settings are updated
